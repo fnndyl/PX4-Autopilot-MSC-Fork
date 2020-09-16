zebbarry/Firmware

mavsdk_tests: add test for stuck mag

src/modules/simulator/simulator_mavlink.cpp
@@ -219,7 +219,15 @@ void Simulator::update_sensors(const hrt_abstime &time, const mavlink_hil_sensor
 
 	// magnetometer
 	if ((sensors.fields_updated & SensorSource::MAG) == SensorSource::MAG && !_mag_blocked) {
-		_px4_mag.update(time, sensors.xmag, sensors.ymag, sensors.zmag);
+		if (_mag_stuck) {
+			_px4_mag.update(time, _last_magx, _last_magy, _last_magz);
+
+		} else {
+			_px4_mag.update(time, sensors.xmag, sensors.ymag, sensors.zmag);
+			_last_magx = sensors.xmag;
+			_last_magy = sensors.ymag;
+			_last_magz = sensors.zmag;
+		}
 	}
 
 	// baro
@@ -957,6 +965,11 @@ void Simulator::check_failure_injections()
 			if (failure_type == vehicle_command_s::FAILURE_TYPE_OFF) {
 				supported = true;
 				_mag_blocked = true;
+
+			} else if (failure_type == vehicle_command_s::FAILURE_TYPE_STUCK) {
+				supported = true;
+				_mag_stuck = true;
+				_mag_blocked = false;
 
 			} else if (failure_type == vehicle_command_s::FAILURE_TYPE_OK) {
 				supported = true;

fnndyl/PX4-Autopilot-MSC-Fork

src/modules/simulator/simulator_mavlink.cpp
@@ -247,8 +247,17 @@ void Simulator::update_sensors(const hrt_abstime &time, const mavlink_hil_sensor
 
 	// magnetometer
 	if ((sensors.fields_updated & SensorSource::MAG) == SensorSource::MAG && !_mag_blocked) {
-		_px4_mag_0.update(time, sensors.xmag, sensors.ymag, sensors.zmag);
-		_px4_mag_1.update(time, sensors.xmag, sensors.ymag, sensors.zmag);
+		if (_mag_stuck) {
+			_px4_mag_0.update(time, _last_magx, _last_magy, _last_magz);
+			_px4_mag_1.update(time, _last_magx, _last_magy, _last_magz);
+
+		} else {
+			_px4_mag_0.update(time, sensors.xmag, sensors.ymag, sensors.zmag);
+			_px4_mag_1.update(time, sensors.xmag, sensors.ymag, sensors.zmag);
+			_last_magx = sensors.xmag;
+			_last_magy = sensors.ymag;
+			_last_magz = sensors.zmag;
+		}
 	}
 
 	// baro
@@ -988,6 +997,11 @@ void Simulator::check_failure_injections()
 			if (failure_type == vehicle_command_s::FAILURE_TYPE_OFF) {
 				supported = true;
 				_mag_blocked = true;
+
+			} else if (failure_type == vehicle_command_s::FAILURE_TYPE_STUCK) {
+				supported = true;
+				_mag_stuck = true;
+				_mag_blocked = false;
 
 			} else if (failure_type == vehicle_command_s::FAILURE_TYPE_OK) {
 				supported = true;