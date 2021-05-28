zevaaero/PX4-ZEVA

TECS: in airspeed-less mode, set airspeed estimate to cruise airspeed
In airspeed-less mode, instead of the average of min/max airspeed, take the cruise
airspeed (from param) for current airspeed estimate. A diff of the airspeed setpoint
from this value results in increased enegery demand (incerase of throttle) even
in airspeed-less mode.

Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

src/lib/tecs/TECS.cpp
@@ -119,10 +119,9 @@ void TECS::_update_speed_states(float airspeed_setpoint, float indicated_airspee
 	_TAS_max   = _indicated_airspeed_max * EAS2TAS;
 	_TAS_min   = _indicated_airspeed_min * EAS2TAS;
 
-	// If airspeed measurements are not being used, fix the airspeed estimate to halfway between
-	// min and max limits
+	// If airspeed measurements are not being used, fix the airspeed estimate to the nominal cruise airspeed
 	if (!PX4_ISFINITE(indicated_airspeed) || !airspeed_sensor_enabled()) {
-		_EAS = 0.5f * (_indicated_airspeed_min + _indicated_airspeed_max);
+		_EAS = _indicated_airspeed_cruise;
 
 	} else {
 		_EAS = indicated_airspeed;

src/modules/fw_pos_control_l1/FixedwingPositionControl.cpp
@@ -96,6 +96,7 @@ FixedwingPositionControl::parameters_update()
 	_tecs.set_max_climb_rate(_param_fw_t_clmb_max.get());
 	_tecs.set_max_sink_rate(_param_fw_t_sink_max.get());
 	_tecs.set_speed_weight(_param_fw_t_spdweight.get());
+	_tecs.set_indicated_airspeed_cruise(_param_fw_airspd_cruise.get());
 	_tecs.set_indicated_airspeed_min(_param_fw_airspd_min.get());
 	_tecs.set_indicated_airspeed_max(_param_fw_airspd_max.get());
 	_tecs.set_min_sink_rate(_param_fw_t_sink_min.get());

fnndyl/PX4-Autopilot-MSC-Fork

src/lib/tecs/TECS.cpp
@@ -116,9 +116,9 @@ void TECS::_update_speed_states(float equivalent_airspeed_setpoint, float equiva
 	_TAS_max   = _equivalent_airspeed_max * EAS2TAS;
 	_TAS_min   = _equivalent_airspeed_min * EAS2TAS;
 
-	// If airspeed measurements are not being used, fix the EAS estimate to halfway between min and max limits
+	// If airspeed measurements are not being used, fix the airspeed estimate to the nominal cruise airspeed
 	if (!PX4_ISFINITE(equivalent_airspeed) || !airspeed_sensor_enabled()) {
-		_EAS = 0.5f * (_equivalent_airspeed_min + _equivalent_airspeed_max);
+		_EAS = _equivalent_airspeed_cruise;
 
 	} else {
 		_EAS = equivalent_airspeed;

src/modules/fw_pos_control_l1/FixedwingPositionControl.cpp
@@ -109,6 +109,7 @@ FixedwingPositionControl::parameters_update()
 	_tecs.set_max_climb_rate(_param_fw_t_clmb_max.get());
 	_tecs.set_max_sink_rate(_param_fw_t_sink_max.get());
 	_tecs.set_speed_weight(_param_fw_t_spdweight.get());
+	_tecs.set_equivalent_airspeed_cruise(_param_fw_airspd_trim.get());
 	_tecs.set_equivalent_airspeed_min(_param_fw_airspd_min.get());
 	_tecs.set_equivalent_airspeed_max(_param_fw_airspd_max.get());
 	_tecs.set_min_sink_rate(_param_fw_t_sink_min.get());