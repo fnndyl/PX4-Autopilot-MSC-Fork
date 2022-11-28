zhengjinzhong/Firmware

mavlink_receiver: refactor forgotten manual_control_setpoint naming

src/modules/airship_att_control/airship_att_control_main.cpp
@@ -91,9 +91,9 @@ AirshipAttitudeControl::publish_actuator_controls()
 
 	} else {
 		_actuators.control[0] = 0.0f;
-		_actuators.control[1] = _manual_control_sp.x;
-		_actuators.control[2] = _manual_control_sp.r;
-		_actuators.control[3] = _manual_control_sp.z;
+		_actuators.control[1] = _manual_control_setpoint.x;
+		_actuators.control[2] = _manual_control_setpoint.r;
+		_actuators.control[3] = _manual_control_setpoint.z;
 	}
 
 	// note: _actuators.timestamp_sample is set in AirshipAttitudeControl::Run()
@@ -126,8 +126,8 @@ AirshipAttitudeControl::Run()
 		publish_actuator_controls();
 
 		/* check for updates in manual control topic */
-		if (_manual_control_sp_sub.updated()) {
-			_manual_control_sp_sub.update(&_manual_control_sp);
+		if (_manual_control_setpoint_sub.updated()) {
+			_manual_control_setpoint_sub.update(&_manual_control_setpoint);
 		}
 
 		/* check for updates in vehicle status topic */

src/modules/mavlink/mavlink_receiver.cpp
@@ -2048,16 +2048,16 @@ MavlinkReceiver::handle_message_manual_control(mavlink_message_t *msg)
 		_rc_pub.publish(rc);
 
 	} else {
-		manual_control_setpoint_s manual{};
+		manual_control_setpoint_s manual_control_setpoint{};
 
-		manual.timestamp = hrt_absolute_time();
-		manual.x = man.x / 1000.0f;
-		manual.y = man.y / 1000.0f;
-		manual.r = man.r / 1000.0f;
-		manual.z = man.z / 1000.0f;
-		manual.data_source = manual_control_setpoint_s::SOURCE_MAVLINK_0 + _mavlink->get_instance_id();
+		manual_control_setpoint.timestamp = hrt_absolute_time();
+		manual_control_setpoint.x = man.x / 1000.0f;
+		manual_control_setpoint.y = man.y / 1000.0f;
+		manual_control_setpoint.r = man.r / 1000.0f;
+		manual_control_setpoint.z = man.z / 1000.0f;
+		manual_control_setpoint.data_source = manual_control_setpoint_s::SOURCE_MAVLINK_0 + _mavlink->get_instance_id();
 
-		_manual_control_setpoint_pub.publish(manual);
+		_manual_control_setpoint_pub.publish(manual_control_setpoint);
 	}
 }
 

fnndyl/PX4-Autopilot-MSC-Fork

src/modules/airship_att_control/airship_att_control_main.cpp
@@ -91,9 +91,9 @@ AirshipAttitudeControl::publish_actuator_controls()
 
 	} else {
 		_actuator_controls.control[0] = 0.0f;
-		_actuator_controls.control[1] = _manual_control_sp.x;
-		_actuator_controls.control[2] = _manual_control_sp.r;
-		_actuator_controls.control[3] = _manual_control_sp.z;
+		_actuator_controls.control[1] = _manual_control_setpoint.x;
+		_actuator_controls.control[2] = _manual_control_setpoint.r;
+		_actuator_controls.control[3] = _manual_control_setpoint.z;
 	}
 
 	// note: _actuator_controls.timestamp_sample is set in AirshipAttitudeControl::Run()
@@ -150,7 +150,7 @@ AirshipAttitudeControl::Run()
 		publishThrustSetpoint(angular_velocity.timestamp_sample);
 
 		/* check for updates in manual control topic */
-		_manual_control_sp_sub.update(&_manual_control_sp);
+		_manual_control_setpoint_sub.update(&_manual_control_setpoint);
 
 		/* check for updates in vehicle status topic */
 		_vehicle_status_sub.update(&_vehicle_status);

src/modules/mavlink/mavlink_receiver.cpp
@@ -2129,22 +2129,22 @@ MavlinkReceiver::handle_message_rc_channels_override(mavlink_message_t *msg)
 void
 MavlinkReceiver::handle_message_manual_control(mavlink_message_t *msg)
 {
-	mavlink_manual_control_t man;
-	mavlink_msg_manual_control_decode(msg, &man);
+	mavlink_manual_control_t mavlink_manual_control;
+	mavlink_msg_manual_control_decode(msg, &mavlink_manual_control);
 
 	// Check target
-	if (man.target != 0 && man.target != _mavlink->get_system_id()) {
+	if (mavlink_manual_control.target != 0 && mavlink_manual_control.target != _mavlink->get_system_id()) {
 		return;
 	}
 
-	manual_control_setpoint_s manual{};
-	manual.x = man.x / 1000.0f;
-	manual.y = man.y / 1000.0f;
-	manual.r = man.r / 1000.0f;
-	manual.z = man.z / 1000.0f;
-	manual.data_source = manual_control_setpoint_s::SOURCE_MAVLINK_0 + _mavlink->get_instance_id();
-	manual.timestamp = manual.timestamp_sample = hrt_absolute_time();
-	_manual_control_input_pub.publish(manual);
+	manual_control_setpoint_s manual_control_setpoint{};
+	manual_control_setpoint.x = mavlink_manual_control.x / 1000.0f;
+	manual_control_setpoint.y = mavlink_manual_control.y / 1000.0f;
+	manual_control_setpoint.r = mavlink_manual_control.r / 1000.0f;
+	manual_control_setpoint.z = mavlink_manual_control.z / 1000.0f;
+	manual_control_setpoint.data_source = manual_control_setpoint_s::SOURCE_MAVLINK_0 + _mavlink->get_instance_id();
+	manual_control_setpoint.timestamp = manual_control_setpoint.timestamp_sample = hrt_absolute_time();
+	_manual_control_input_pub.publish(manual_control_setpoint);
 }
 
 void